2b23ede60bacfc056346adb4d476650a8d808fc5
wangleiR <1220632559@qq.com>
2018-04-21 15:03:43 +0800

    completed the RandomIntGeneratorTest

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,42 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
 
+    public InputValidator inputValidator = new InputValidator();
+
+    @Test
+    public void should_get_right_with_correct_input() {
+        //输入正确时输入时,输入验证通过
+        String str = "1 2 3 4";
+        assertEquals(inputValidator.validate(str),true);
+    }
+
+    @Test
+    public void should_get_error_with_long_size_input() {
+        //输入数字的个数不正确时,输入验证不通过
+        String str = "1 2 3 4 5";
+        assertEquals(inputValidator.validate(str),false);
+    }
+
+    @Test
+    public void should_get_error_with_other_split_input() {
+        //输入数字的分隔符不正确时,输入验证不通过
+        String str = "1;2 3;4";
+        assertEquals(inputValidator.validate(str),false);
+    }
+
+    @Test
+    public void should_get_error_with_repeat_num_input() {
+        //输入重复数字时,输入验证不通过
+        String str = "1 2 2 4";
+        assertEquals(inputValidator.validate(str),false);
+    }
 }
--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,36 @@
 package tw.core;
 
 
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
 
+    @Test
+    public void should_get_right_with_correct_input() {
+        //产生随机数
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+
+        when(randomIntGenerator.generateNums(9,4)).thenReturn("4 7 1 6");
+        assertEquals(randomIntGenerator.generateNums(9,4),"4 7 1 6");
+    }
+
+    @Test
+    public void return_answer_in_fixed_num() {
+        //产生指定个数的随机数
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        assertEquals(Arrays.stream(randomIntGenerator.generateNums(9,4).split(" "))
+                .collect(Collectors.toList()).size() == 4 ? true : false , true);
+    }
 }
