49a959fcfc3b8b99cb180e11ed3843c9520680c0
wangleiR <1220632559@qq.com>
2018-04-21 22:56:35 +0800

    completed the AnswerGeneratorTest

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,27 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+
+    @Test
+    public void should_get_right_in_generateAnswer() throws Exception{
+
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        when(randomIntGenerator.generateNums(9,4)).thenReturn("4 7 1 6");
+        assertEquals(answerGenerator.generate().toString(),"4 7 1 6");
+
+    }
 
 }
 
